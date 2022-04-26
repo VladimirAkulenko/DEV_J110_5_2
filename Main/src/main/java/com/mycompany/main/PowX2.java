e56b4084c28d5e762c48d792dd503451f0d31129
USER <USER@LAPTOP-E8HIQOR6>
2022-04-26 22:46:14 +0300

    Выполнение лабораторной работы 5 часть вторая - Вычисление интеграла заданной функции методом прямоугольников

---
+++ Main/src/main/java/com/mycompany/main/PowX2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main;
+
+/**
+ *
+ * @author USER
+ */
+public class PowX2 implements Function{
+    @Override
+    public double getFunc(double x) {
+        return Math.pow(x,2);
+    }
+}
